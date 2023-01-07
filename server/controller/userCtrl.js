97dc76f7b761bb450c9189695a996389088a9122
TheBarsoom <117264166+TheBarsoom@users.noreply.github.com>
2023-01-07 16:23:55 +0500

    S-5 ADD Bcrypt module

--- server/controller/userCtrl.js
+++ server/controller/userCtrl.js
@@ -1,18 +1,21 @@
 const User =require("../models/userModel")
+const asyncHandler = require("express-async-handler")
 
-
-const createUser=async(req,res)=>{
-    const {email}=req.body
-    const findUser=await User.findOne({email:email})
-    if(!findUser){
-        const newUser = await User.create(req.body)
-        res.json(newUser)
-    }else{
-        res.json({
-            msg:"User already exist",
-            success:false 
-        })
+const createUser=asyncHandler(
+    async(req,res)=>{
+        const {email}=req.body
+        const findUser=await User.findOne({email:email})
+        if(!findUser){
+            const newUser = await User.create(req.body)
+            res.json(newUser)
+        }else{
+            throw new Error ("User already exist",)
+            
+              
+          
+        
+        }
     }
-}
-
+    
+)
 module.exports={createUser}
--- server/index.js
+++ server/index.js
@@ -1,21 +1,22 @@
-const express = require("express")
-const app  = express();
-const dotenv = require("dotenv").config()
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const authRouter = require("./routes/authRoute")
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv").config();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const authRouter = require("./routes/authRoute");
 const PORT = process.env.PORT || 4000;
 
-mongoose.set('strictQuery', true);
-const dbConnect=require("./config/dbConnect");
+mongoose.set("strictQuery", true);
+const dbConnect = require("./config/dbConnect");
 const { notFound, errorHandler } = require("./middlewares/errorHandler");
+
 dbConnect();
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-
-
-app.use("/api/user",authRouter)
-app.use(notFound)
-app.use(errorHandler)
-app.listen(PORT,()=>{console.log(`SERVER RUNNING PORT: ${PORT}`);})
+app.use("/api/user", authRouter);
+app.use(notFound);
+app.use(errorHandler);
+app.listen(PORT, () => {
+  console.log(`SERVER RUNNING PORT: ${PORT}`);
+});

b6764a6095f9fa249cb43a30cb8e3b0cdaed7bd9
TheBarsoom <117264166+TheBarsoom@users.noreply.github.com>
2023-01-07 16:02:47 +0500

    S-3 create-user-model/controller/route

--- server/index.js
+++ server/index.js
@@ -2,12 +2,17 @@ const express = require("express")
 const app  = express();
 const dotenv = require("dotenv").config()
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const authRouter = require("./routes/authRoute")
 const PORT = process.env.PORT || 4000;
 
 mongoose.set('strictQuery', true);
 const dbConnect=require("./config/dbConnect")
 dbConnect();
-app.use("/",(req,res)=>{
-    res.send("Barsoom")
-})
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+
+
+app.use("/api/user",authRouter)
 app.listen(PORT,()=>{console.log(`SERVER RUNNING PORT: ${PORT}`);})

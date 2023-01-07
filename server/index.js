306b55b71aef6127beb816a58c14c2d72ae6186c
TheBarsoom <117264166+TheBarsoom@users.noreply.github.com>
2023-01-07 16:08:53 +0500

    S-4 Create middleware (notFound errorHandler)

--- server/index.js
+++ server/index.js
@@ -7,7 +7,8 @@ const authRouter = require("./routes/authRoute")
 const PORT = process.env.PORT || 4000;
 
 mongoose.set('strictQuery', true);
-const dbConnect=require("./config/dbConnect")
+const dbConnect=require("./config/dbConnect");
+const { notFound, errorHandler } = require("./middlewares/errorHandler");
 dbConnect();
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
@@ -15,4 +16,6 @@ app.use(bodyParser.urlencoded({extended:true}))
 
 
 app.use("/api/user",authRouter)
+app.use(notFound)
+app.use(errorHandler)
 app.listen(PORT,()=>{console.log(`SERVER RUNNING PORT: ${PORT}`);})

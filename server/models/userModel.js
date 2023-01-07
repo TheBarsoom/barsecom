e556f3831c3a04d2c21b6b18bb7fc6e6731638c3
TheBarsoom <117264166+TheBarsoom@users.noreply.github.com>
2023-01-07 21:19:30 +0500

    S-11 Add AuthMiddleware/Updated UserModel

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,39 +1,48 @@
-const mongoose = require("mongoose")
-const { generateHash, compareHash } = require("../modules/bcrypt")
-const userSchema= new mongoose.Schema({
-    firstname:{
-        type:String,
-        required:true,
+const mongoose = require("mongoose");
+const { ObjectId } = require("mongoose");
+const { generateHash, compareHash } = require("../modules/bcrypt");
+const userSchema = new mongoose.Schema(
+  {
+    firstname: {
+      type: String,
+      required: true,
     },
-    lastname:{
-        type:String,
-        required:true,
+    lastname: {
+      type: String,
+      required: true,
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    mobile:{
-        type:String,
-        required:true,
+    mobile: {
+      type: String,
+      required: true,
     },
-    password:{
-        type:String,
-        required:true,
+    password: {
+      type: String,
+      required: true,
     },
-    role:{
-        type:String,
-        default:"user",
+    role: {
+      type: String,
+      default: "user",
     },
-
-})
+    cart: {
+      type: Array,
+      default: [],
+    },
+    wishlist: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
+    address: [{ type:mongoose.Schema.Types.ObjectId, ref: "Address" }],
+  },
+  { timestamps: true }
+);
 
 // userSchema.pre(`save`,async function (next){
 //     this.password = await generateHash(this.password)
 // })
 
-userSchema.methods.isPasswordMatched = async function (enteredPassword){
-    return compareHash(enteredPassword,this.password)
-}
-module.exports=mongoose.model("User",userSchema)
+userSchema.methods.isPasswordMatched = async function (enteredPassword) {
+  return compareHash(enteredPassword, this.password);
+};
+module.exports = mongoose.model("User", userSchema);
--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,12 +1,13 @@
 const express= require("express")
 const { createUser ,loginUser, getallUser,getOneUser,deleteUser,updateUser} = require("../controller/userCtrl")
+const { authMiddleware } = require("../middlewares/authMid")
 const router = express.Router()
 
 router.post("/register",createUser)
 router.post("/login",loginUser)
 router.get("/all-users",getallUser)
-router.get("/:id",getOneUser)
-router.delete("/:id",deleteUser)
+router.get("/:id",authMiddleware,getOneUser)
+router.delete("/:id", deleteUser)
 router.put("/:id",updateUser)
 module.exports=router
 

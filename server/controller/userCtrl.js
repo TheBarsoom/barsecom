329265163b199e1f7aec6ad239de00b2f2947655
TheBarsoom <117264166+TheBarsoom@users.noreply.github.com>
2023-01-07 18:06:08 +0500

    S-8 Add All user route

--- server/controller/userCtrl.js
+++ server/controller/userCtrl.js
@@ -1,55 +1,56 @@
-const User =require("../models/userModel")
-const asyncHandler = require("express-async-handler")
-const { generateHash, compareHash } = require("../modules/bcrypt")
-const { createToken } = require("../modules/jwt")
+const User = require("../models/userModel");
+const asyncHandler = require("express-async-handler");
+const { generateHash, compareHash } = require("../modules/bcrypt");
+const { createToken } = require("../modules/jwt");
 
-const createUser=asyncHandler(
-    async(req,res)=>{
-        const {email}=req.body
-        const findUser=await User.findOne({email:email})
-        if(!findUser){
-            const newUser = await User.create({...req.body, password:await generateHash(req.body.password)})
-            res.json(newUser)
-        }else{
-            throw new Error ("User already exist",)
-            
-              
-          
-        
-        }
-    }
-    
-)
+const createUser = asyncHandler(async (req, res) => {
+  const { email } = req.body;
+  const findUser = await User.findOne({ email: email });
+  if (!findUser) {
+    const newUser = await User.create({
+      ...req.body,
+      password: await generateHash(req.body.password),
+    });
+    res.json(newUser);
+  } else {
+    throw new Error("User already exist");
+  }
+});
 
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  console.log(email, password);
 
-const loginUser=asyncHandler(
-    async(req,res)=>{
-        const {email,password}=req.body
-        console.log(email,password);
-        
-        const findUser=await User.findOne({email:email})
-        // if(findUser && await findUser.isPasswordMatched(password)){
-        // res.json(findUser)
-        // }else{
-        //     throw new Error("Password is incorrect");
-        // }
-        
-        if(!findUser)  throw new Error ("User does not exist",)
+  const findUser = await User.findOne({ email: email });
+  // if(findUser && await findUser.isPasswordMatched(password)){
+  // res.json(findUser)
+  // }else{
+  //     throw new Error("Password is incorrect");
+  // }
 
-        const isTrust=await compareHash(password,findUser.password)
-        console.log(isTrust);
+  if (!findUser) throw new Error("User does not exist");
 
-        if (!isTrust) throw new Error("Password is incorrect");
-        res.json({
-            _id:findUser?._id,
-            firstname:findUser?.firstname,
-            lastname:findUser?.email,
-            mobile:findUser?.mobile,
-            // token:await createToken({id:findUser?._id})
-            token:await createToken(findUser?._id)
+  const isTrust = await compareHash(password, findUser.password);
+  console.log(isTrust);
 
-        })
-    }
-    
-)
-module.exports={createUser,loginUser}
+  if (!isTrust) throw new Error("Password is incorrect");
+  res.json({
+    _id: findUser?._id,
+    firstname: findUser?.firstname,
+    lastname: findUser?.email,
+    mobile: findUser?.mobile,
+    // token:await createToken({id:findUser?._id})
+    token: await createToken(findUser?._id),
+  });
+});
+
+const getallUser = asyncHandler(async (req, res) => {
+  try {
+    const getUsers = await User.find();
+    res.json(getAllUsers);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+module.exports = { createUser, loginUser, getallUser };
--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,9 +1,9 @@
 const express= require("express")
-const { createUser ,loginUser} = require("../controller/userCtrl")
+const { createUser ,loginUser, getallUser} = require("../controller/userCtrl")
 const router = express.Router()
 
 router.post("/register",createUser)
 router.post("/login",loginUser)
-
+router.post("/all-users",getallUser)
 module.exports=router
 
